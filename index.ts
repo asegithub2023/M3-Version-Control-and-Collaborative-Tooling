772f99695aaf1badb7fed013eea5a63eabb1b48a
Asefa Negash <aseaau21@gmail.com>
2026-06-07 20:34:16 +0300

    conflict one: change student id

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import { EnrollmentStatus, describeEnrollment } from "./models/enrollment.model"
 
 
 const student: Student = {
-id: "STU-001B",
+id: "STU-001-A",
 name: "Hana Tadesse",
 enrollmentDate: Temporal.Now.instant(),
 };
